2f78bd53e00604438f68d6b88940b06a5bb9e109
DanielRabl <danielrabl1999@gmail.com>
2023-02-21 20:30:19 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,157 +1,54 @@
 #include <qpl/qpl.hpp>
 
-qpl::size get_flipped_bits(const std::string& a, const std::string& b) {
-	qpl::size result = 0u;
-	for (qpl::size i = 0u; i < a.length(); ++i) {
-		result += qpl::number_of_set_bits(qpl::u8_cast(a[i]) ^ qpl::u8_cast(b[i]));
-	}
-	return result;
-}
 
-qpl::f64 get_bits_avalance(const std::string& a, const std::string& b) {
-	auto flipped = get_flipped_bits(a, b);
-	auto total_bits = a.length() * qpl::bits_in_type<qpl::char_type>();
-
-	auto rate = flipped / qpl::f64_cast(total_bits);
-	return rate;
-}
 
 namespace crypto {
 	constexpr qpl::cipher_config config{
 		.N = 4,
-		.cipher_rounds = 1,
+		.cipher_rounds = 3,
 		.key_size = 64,
 		.table_size = 64,
-		.bidirectional = false,
+		.skip_rotation_chance = 0.0,
+		.bidirectional = true,
+		.initialization_vector = true
 	};
 	qpl::cipherN<config> cipher;
-}
-namespace crypto2 {
-	constexpr qpl::cipher_config config{
-		.N = 4,
-		.cipher_rounds = 1,
-		.key_size = 64,
-		.table_size = 64,
-		.bidirectional = false,
-	};
-	qpl::cipherN<config> cipher;
+	qpl::cipherN<config> cipher2;
 }
 
 std::string encrypted(const std::string& message, const std::string& key) {
-	return crypto::cipher.encrypt(message, key);
+	return crypto::cipher.encrypted(message, key);
 }
 std::string aes_encrypted(const std::string& message, const std::string& key) {
 	return qpl::encrypted_keep_size(message, key);
 }
 
-struct avalanche {
-	void update(std::string message, std::string key, bool use_aes) {
-		qpl::f64 sum_a = 0.0;
-
-		auto encrypt = use_aes ? aes_encrypted : encrypted;
-
-		auto encrypted = encrypt(message, key);
-		this->encrypted_sum += get_bits_avalance(message, encrypted);
-
-		{
-			auto key1 = key;
-			qpl::toggle_bit(key1[0], qpl::bits_in_type<qpl::char_type>() - 1);
-		
-			auto key1_encrypted = encrypt(message, key1);
-			this->key1_sum += get_bits_avalance(key1_encrypted, encrypted);
-		}
-		
-		{
-			auto key2 = key;
-			qpl::toggle_bit(key2[key.length() / 2], qpl::bits_in_type<qpl::char_type>() / 2);
-		
-			auto key2_encrypted = encrypt(message, key2);
-			this->key2_sum += get_bits_avalance(key2_encrypted, encrypted);
-		}
-		
-		{
-			auto key3 = key;
-			qpl::toggle_bit(key3[key.length() - 1], 0u);
-		
-			auto key3_encrypted = encrypt(message, key3);
-			this->key3_sum += get_bits_avalance(key3_encrypted, encrypted);
-		}
-
-		{
-			auto msg1 = message;
-			qpl::toggle_bit(msg1[0], qpl::bits_in_type<qpl::char_type>() - 1);
-
-			auto msg1_encrypted = encrypt(msg1, key);
-			this->msg1_sum += get_bits_avalance(msg1_encrypted, encrypted);
-		}
-		{
-			auto msg2 = message; 
-			qpl::toggle_bit(msg2[msg2.length() / 2], qpl::bits_in_type<qpl::char_type>() / 2);
-
-			auto msg2_encrypted = encrypt(msg2, key);
-			this->msg2_sum += get_bits_avalance(msg2_encrypted, encrypted);
-		}
-		{
-			auto msg3 = message;
-			qpl::toggle_bit(msg3[msg3.length() - 1], 0u);
-
-			auto msg3_encrypted = encrypt(msg3, key);
-			this->msg3_sum += get_bits_avalance(msg3_encrypted, encrypted);
-		}
-
-		++this->ctr;
-	}
-
-	void print(bool use_aes) {
-		qpl::println(use_aes ? "AES " : "MY  ", "msg vs encrypted : ", qpl::percentage_string_precision(this->encrypted_sum / this->ctr, 2));
-		qpl::println(use_aes ? "AES " : "MY  ", "key[<] flipped   : ", qpl::percentage_string_precision(this->key1_sum / this->ctr, 2));
-		qpl::println(use_aes ? "AES " : "MY  ", "key[m] flipped   : ", qpl::percentage_string_precision(this->key2_sum / this->ctr, 2));
-		qpl::println(use_aes ? "AES " : "MY  ", "key[>] flipped   : ", qpl::percentage_string_precision(this->key3_sum / this->ctr, 2));
-		qpl::println(use_aes ? "AES " : "MY  ", "msg[<] flipped   : ", qpl::percentage_string_precision(this->msg1_sum / this->ctr, 2));
-		qpl::println(use_aes ? "AES " : "MY  ", "msg[m] flipped   : ", qpl::percentage_string_precision(this->msg2_sum / this->ctr, 2));
-		qpl::println(use_aes ? "AES " : "MY  ", "msg[>] flipped   : ", qpl::percentage_string_precision(this->msg3_sum / this->ctr, 2));
-		qpl::println();
-	}
-
-	qpl::f64 encrypted_sum = 0.0;
-	qpl::f64 key1_sum = 0.0;
-	qpl::f64 key2_sum = 0.0;
-	qpl::f64 key3_sum = 0.0;
-	qpl::f64 msg1_sum = 0.0;
-	qpl::f64 msg2_sum = 0.0;
-	qpl::f64 msg3_sum = 0.0;
-	qpl::size ctr = 0u;
-};
-
-namespace aval {
-	avalanche mine;
-	//avalanche aes;
-}
-
-void check_avalanche(const std::string& message, const std::string& key) {
-	aval::mine.update(message, key, false);
-	//aval::aes.update(message, key, true);
-}
-
-
-void check_encryption(std::string message, std::string key) {
+void check_encryption(std::string& message, std::string key) {
+	auto before = message;
 	qpl::begin_benchmark("encrypt");
-	auto encrypted = crypto::cipher.encrypt(message, key);
+
+	crypto::cipher.encrypt(message, key);
+
+
+	//if (encrypted != message) {
+	//	//qpl::println("message   : ", qpl::hex_string(before));
+	//	qpl::println("encrypted : ", qpl::hex_string(encrypted));
+	//	qpl::println("decrypted : ", qpl::hex_string(message));
+	//	qpl::println();
+	//}
+
+
 	qpl::begin_benchmark_end_previous("decrypt");
-	auto decrypted = crypto2::cipher.decrypt(encrypted, key);
+	//auto decrypted = crypto::cipher.decrypted(encrypted, key);
+	crypto::cipher.decrypt(message, key);
 	qpl::end_benchmark();
 
-	if (decrypted != message) {
-		qpl::println("message   : ", qpl::hex_string(message));
-		qpl::println("encrypted : ", qpl::hex_string(encrypted));
-		qpl::println("decrypted : ", qpl::hex_string(decrypted));
+	if (before != message) {
+		qpl::println("message   : ", qpl::hex_string(before));
+		//qpl::println("encrypted : ", qpl::hex_string(encrypted));
+		qpl::println("decrypted : ", qpl::hex_string(message));
 		qpl::println();
 	}
-
-	//qpl::begin_benchmark("AES");
-	//encrypted = qpl::encrypted_keep_size(message, key);
-	//decrypted = qpl::decrypted_keep_size(encrypted, key);
-	//qpl::end_benchmark();
 }
 
 void check_mistakes() {
@@ -160,22 +57,20 @@ void check_mistakes() {
 	qpl::size bytes = 0u;
 
 	auto key = qpl::get_random_string_full_range(crypto::cipher.key_size);
-	//for (qpl::size i = 10; i < key.length(); ++i) {
-	//	key[i] = 0u;
-	//}
 
+	std::string message;
 	for (qpl::size i = 0u;; ++i) {
-		auto l = 64 * 2;
-		auto message = qpl::get_random_string_with_repetions(l, 64);
+		//auto l = 64 * 2;
+		constexpr auto l = qpl::mebibyte(16);
+		message = qpl::get_random_string_with_repetions(l, 64);
 
 		check_encryption(message, key);
-		//check_avalanche(message, key);
+		//check_encryption2(message, key);
 		bytes += message.length();
 
 		if (qpl::get_time_signal(0.5)) {
 			auto byte_rate = qpl::size_cast(qpl::f64_cast(bytes) / clock.elapsed_f());
 			qpl::println(qpl::memory_size_string(bytes), " (", qpl::memory_size_string(byte_rate), " / sec)");
-			//aval::mine.print(false);
 			qpl::print_benchmark();
 		}
 	}
@@ -251,7 +146,6 @@ void make_tables() {
 	qpl::println(inv_string);
 }
 
-
 void create_output() {
 	//auto key = "21q1aN4TrJU5XOPF4YE532H10FD03I8F";
 	auto key = qpl::get_random_string_full_range(64);
@@ -268,8 +162,10 @@ void create_output() {
 }
 
 int main() try {
-	//mds();
-	//create_output();
+
+	std::string string = "hello world 123125678 hello world 123125678 hello world 123125678";
+	check_encryption(string, "123456");
+
 	check_mistakes();
 }
 catch (std::exception& any) {
